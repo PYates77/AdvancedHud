cd1c25690f4bd55d5e8099e842f8a9c339b6a003
Paul <Paul.maxyat@gatech.edu>
2017-02-10 17:55:07 -0500

    Revert "added additional bluetooth functionality"
    This reverts commit 948d74a50e3108d9a890eb3d3e856b1e4972d648.

--- BluetoothTest/app/src/main/java/paul/bluetoothtest/MainActivity.java
+++ BluetoothTest/app/src/main/java/paul/bluetoothtest/MainActivity.java
@@ -74,4 +74,58 @@ public class MainActivity extends AppCompatActivity {
         // Don't forget to unregister the ACTION_FOUND receiver.
         unregisterReceiver(mReceiver);
     }
+<<<<<<< HEAD
+=======
+
+    private class AcceptThread extends Thread {
+        String NAME;
+        UUID MY_UUID;
+        String MY_TAG = "AcceptThread";
+
+
+        private final BluetoothServerSocket mmServerSocket;
+
+        public AcceptThread() {
+            // Use a temporary object that is later assigned to mmServerSocket
+            // because mmServerSocket is final.
+            BluetoothServerSocket tmp = null;
+            try {
+                // MY_UUID is the app's UUID string, also used by the client code.
+                tmp = mBluetoothAdapter.listenUsingRfcommWithServiceRecord(NAME, MY_UUID);
+            } catch (IOException e) {
+                Log.e(MY_TAG, "Socket's listen() method failed", e);
+            }
+            mmServerSocket = tmp;
+        }
+
+        public void run() {
+            BluetoothSocket socket = null;
+            // Keep listening until exception occurs or a socket is returned.
+            while (true) {
+                try {
+                    socket = mmServerSocket.accept();
+                } catch (IOException e) {
+                    Log.e(MY_TAG, "Socket's accept() method failed", e);
+                    break;
+                }
+
+                if (socket != null) {
+                    // A connection was accepted. Perform work associated with
+                    // the connection in a separate thread.
+                    //manageMyConnectedSocket(socket);
+                    cancel();
+                }
+            }
+        }
+
+        // Closes the connect socket and causes the thread to finish.
+        public void cancel() {
+            try {
+                mmServerSocket.close();
+            } catch (IOException e) {
+                Log.e(MY_TAG, "Could not close the connect socket", e);
+            }
+        }
+    }
+>>>>>>> parent of 948d74a... added additional bluetooth functionality
 }
