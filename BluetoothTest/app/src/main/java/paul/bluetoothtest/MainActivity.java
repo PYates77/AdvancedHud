948d74a50e3108d9a890eb3d3e856b1e4972d648
PYates77 <paul.maxyat@gmail.com>
2017-02-10 16:41:40 -0500

    added additional bluetooth functionality

--- BluetoothTest/app/src/main/java/paul/bluetoothtest/MainActivity.java
+++ BluetoothTest/app/src/main/java/paul/bluetoothtest/MainActivity.java
@@ -87,10 +87,67 @@ public class MainActivity extends AppCompatActivity {
         unregisterReceiver(mReceiver);
     }
 
+
+
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+        private final String TAG = "ConnectThread";
+        private final UUID MY_UUID = ;
+
+        public ConnectThread(BluetoothDevice device) {
+            // Use a temporary object that is later assigned to mmSocket
+            // because mmSocket is final.
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+
+            try {
+                // Get a BluetoothSocket to connect with the given BluetoothDevice.
+                // MY_UUID is the app's UUID string, also used in the server code.
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+                Log.e(TAG, "Socket's create() method failed", e);
+            }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            // Cancel discovery because it otherwise slows down the connection.
+            mBluetoothAdapter.cancelDiscovery();
+
+            try {
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                mmSocket.connect();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and return.
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException) {
+                    Log.e(TAG, "Could not close the client socket", closeException);
+                }
+                return;
+            }
+
+            // The connection attempt succeeded. Perform work associated with
+            // the connection in a separate thread.
+            //manageMyConnectedSocket(mmSocket);
+        }
+
+        // Closes the client socket and causes the thread to finish.
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Could not close the client socket", e);
+            }
+        }
+    }
+
     private class AcceptThread extends Thread {
         String NAME;
         UUID MY_UUID;
-        String MY_TAG = "AcceptThread";
+        String TAG = "AcceptThread";
 
 
         private final BluetoothServerSocket mmServerSocket;
@@ -103,7 +160,7 @@ public class MainActivity extends AppCompatActivity {
                 // MY_UUID is the app's UUID string, also used by the client code.
                 tmp = mBluetoothAdapter.listenUsingRfcommWithServiceRecord(NAME, MY_UUID);
             } catch (IOException e) {
-                Log.e(MY_TAG, "Socket's listen() method failed", e);
+                Log.e(TAG, "Socket's listen() method failed", e);
             }
             mmServerSocket = tmp;
         }
@@ -115,7 +172,7 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     socket = mmServerSocket.accept();
                 } catch (IOException e) {
-                    Log.e(MY_TAG, "Socket's accept() method failed", e);
+                    Log.e(TAG, "Socket's accept() method failed", e);
                     break;
                 }
 
@@ -133,7 +190,7 @@ public class MainActivity extends AppCompatActivity {
             try {
                 mmServerSocket.close();
             } catch (IOException e) {
-                Log.e(MY_TAG, "Could not close the connect socket", e);
+                Log.e(TAG, "Could not close the connect socket", e);
             }
         }
     }
