39771ad0c51507476b561065af75729cfe587c0a
Paul <Paul.maxyat@gatech.edu>
2017-02-10 19:08:48 -0500

    bluetooth functionality enhanced

--- BluetoothTest/app/src/main/java/paul/bluetoothtest/MainActivity.java
+++ BluetoothTest/app/src/main/java/paul/bluetoothtest/MainActivity.java
@@ -2,20 +2,49 @@ package paul.bluetoothtest;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView mainText;
+    Button clientButton;
+    Button serverButton;
+    Thread bluetoothThread;
+    Thread socketThread;
+    BluetoothDevice btDevice;
+
+    Handler mHandler;
+
     BluetoothAdapter mBluetoothAdapter;
+
+    private interface MessageConstants {
+        int MESSAGE_READ = 0;
+        int MESSAGE_WRITE = 1;
+        int MESSAGE_TOAST = 2;
+
+        // ... (Add other message types here as needed.)
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
         registerReceiver(mReceiver, filter);
 
         mainText = (TextView) findViewById(R.id.textview_main);
+        clientButton = (Button) findViewById(R.id.button_client);
+        serverButton = (Button) findViewById(R.id.button_server);
 
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null ) {
@@ -37,6 +68,24 @@ public class MainActivity extends AppCompatActivity {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, 0); //TODO: use request code (the argument set to 0) correctly
         }
+        listPairedDevices();
+
+        clientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                bluetoothThread = new ConnectThread(btDevice);
+                bluetoothThread.run();
+            }
+        });
+        serverButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                bluetoothThread = new AcceptThread();
+                bluetoothThread.run();
+            }
+        }));
+
+
     }
 
     private void listPairedDevices(){
@@ -48,6 +97,9 @@ public class MainActivity extends AppCompatActivity {
                 String deviceName = device.getName();
                 String deviceHardwareAddress = device.getAddress(); // MAC address
                 mainText.setText(mainText.getText() + "\n" + deviceName + "\t" + deviceHardwareAddress);
+
+                //go ahead and set the only device (0) to the main device
+                btDevice = device;
             }
         }
     }
@@ -74,8 +126,6 @@ public class MainActivity extends AppCompatActivity {
         // Don't forget to unregister the ACTION_FOUND receiver.
         unregisterReceiver(mReceiver);
     }
-<<<<<<< HEAD
-=======
 
     private class AcceptThread extends Thread {
         String NAME;
@@ -112,6 +162,8 @@ public class MainActivity extends AppCompatActivity {
                 if (socket != null) {
                     // A connection was accepted. Perform work associated with
                     // the connection in a separate thread.
+                    socketThread = new ConnectedThread(socket);
+                    socketThread.run();
                     //manageMyConnectedSocket(socket);
                     cancel();
                 }
@@ -127,5 +179,154 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
->>>>>>> parent of 948d74a... added additional bluetooth functionality
+
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+        private final UUID MY_UUID = UUID.fromString("ce359240-efe7-11e6-9598-0800200c9a66");
+        private final String TAG = "AcceptThread_Debug_Tag";
+
+        public ConnectThread(BluetoothDevice device) {
+            // Use a temporary object that is later assigned to mmSocket
+            // because mmSocket is final.
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+
+            try {
+                // Get a BluetoothSocket to connect with the given BluetoothDevice.
+                // MY_UUID is the app's UUID string, also used in the server code.
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+                Log.e(TAG, "Socket's create() method failed", e);
+            }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            // Cancel discovery because it otherwise slows down the connection.
+            mBluetoothAdapter.cancelDiscovery();
+
+            try {
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                mmSocket.connect();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and return.
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException) {
+                    Log.e(TAG, "Could not close the client socket", closeException);
+                }
+                return;
+            }
+
+            // The connection attempt succeeded. Perform work associated with
+            // the connection in a separate thread.
+            socketThread = new ConnectedThread(mmSocket);
+            socketThread.run();
+
+            //manageMyConnectedSocket(mmSocket);
+            cancel();
+        }
+
+        // Closes the client socket and causes the thread to finish.
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Could not close the client socket", e);
+            }
+        }
+    }
+
+    private class ConnectedThread extends Thread {
+        private final String TAG = "ConnectThread_Debug_Tag";
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+        private byte[] mmBuffer; // mmBuffer store for the stream
+
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams; using temp objects because
+            // member streams are final.
+            try {
+                tmpIn = socket.getInputStream();
+            } catch (IOException e) {
+                Log.e(TAG, "Error occurred when creating input stream", e);
+            }
+            try {
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+                Log.e(TAG, "Error occurred when creating output stream", e);
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            mmBuffer = new byte[1024];
+            int numBytes; // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs.
+            while (true) {
+                try {
+                    // Read from the InputStream.
+                    numBytes = mmInStream.read(mmBuffer);
+                    // Send the obtained bytes to the UI activity.
+                    Message readMsg = mHandler.obtainMessage(
+                            MessageConstants.MESSAGE_READ, numBytes, -1,
+                            mmBuffer);
+                    readMsg.sendToTarget();
+
+                    mainText.setText(new String(mmBuffer));
+
+                    String testMessage = "Hola!";
+                    write(testMessage.getBytes());
+
+                } catch (IOException e) {
+                    Log.d(TAG, "Input stream was disconnected", e);
+                    mainText.setText("Input stream disconnected");
+                    break;
+                }
+            }
+        }
+
+        // Call this from the main activity to send data to the remote device.
+        public void write(byte[] bytes) {
+            try {
+                mmOutStream.write(bytes);
+
+                // Share the sent message with the UI activity.
+                Message writtenMsg = mHandler.obtainMessage(
+                        MessageConstants.MESSAGE_WRITE, -1, -1, mmBuffer);
+                writtenMsg.sendToTarget();
+            } catch (IOException e) {
+                Log.e(TAG, "Error occurred when sending data", e);
+
+                // Send a failure message back to the activity.
+                Message writeErrorMsg =
+                        mHandler.obtainMessage(MessageConstants.MESSAGE_TOAST);
+                Bundle bundle = new Bundle();
+                bundle.putString("toast",
+                        "Couldn't send data to the other device");
+                writeErrorMsg.setData(bundle);
+                mHandler.sendMessage(writeErrorMsg);
+            }
+        }
+
+        // Call this method from the main activity to shut down the connection.
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Could not close the connect socket", e);
+            }
+        }
+    }
+
 }
