470904724ab532efdf652328507430b4774129ea
ymosinzon <yotam@gatech.edu>
2017-02-22 20:22:43 -0500

    Update Plane.java

--- TangoSource/Plane.java
+++ TangoSource/Plane.java
@@ -1,7 +1,29 @@
-public class Plane {
-  public Plane(Point a, Point b, Point c) {
-    Point ab = new Point(a.x - b.x, a.y - b.y, a.z - b.z);
-    Point ac = new Point(a.x - c.x, a.y - c.y, a.z - c.z);
-    Point ab_x_ac = new Point(ab.y*ac.z - ab.z*ac.y);
-  }
+public class Point {
+  
+ private int index = -1; //denotes which Cluster it belongs to
+ public double x, y, z;
+  
+ public Point(double x, double y, double z) {
+  this.x = x;
+  this.y = y;
+  this.z = z;
+ }
+  
+ public Double getSquareOfDistance(Point anotherPoint){
+  return  (x - anotherPoint.x) * (x - anotherPoint.x)
+    + (y - anotherPoint.y) *  (y - anotherPoint.y) 
+    + (z - anotherPoint.z) *  (z - anotherPoint.z);
+ }
+ 
+ public int getIndex() {
+  return index;
+ }
+ 
+ public void setIndex(int index) {
+  this.index = index;
+ }
+  
+ public String toString(){
+  return "(" + x + "," + y + "," + z + ")";
+ } 
 }
