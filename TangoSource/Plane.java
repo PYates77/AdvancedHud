4e83ab9c2a16b48a5f4920e4b43ab79da012ea9a
ymosinzon <yotam@gatech.edu>
2017-02-22 20:23:31 -0500

    Update Plane.java

--- TangoSource/Plane.java
+++ TangoSource/Plane.java
@@ -1,29 +1,21 @@
-public class Point {
+public class Plane {
+  public Point direction;
+  public Point intersect;
   
- private int index = -1; //denotes which Cluster it belongs to
- public double x, y, z;
-  
- public Point(double x, double y, double z) {
-  this.x = x;
-  this.y = y;
-  this.z = z;
- }
-  
- public Double getSquareOfDistance(Point anotherPoint){
-  return  (x - anotherPoint.x) * (x - anotherPoint.x)
-    + (y - anotherPoint.y) *  (y - anotherPoint.y) 
-    + (z - anotherPoint.z) *  (z - anotherPoint.z);
- }
- 
- public int getIndex() {
-  return index;
- }
- 
- public void setIndex(int index) {
-  this.index = index;
- }
-  
- public String toString(){
-  return "(" + x + "," + y + "," + z + ")";
- } 
+  public Plane(Point a, Point b, Point c) {
+    Point ab = new Point(a.x - b.x, a.y - b.y, a.z - b.z);
+    Point ac = new Point(a.x - c.x, a.y - c.y, a.z - c.z);
+    direction = new Point(ab.y*ac.z - ab.z*ac.y, ab.z*ac.x - ab.x*ac.z, ab.x*ac.y - ab.y*ac.x);
+    intersect = a.x*direction*x + a.y*direction*y + a.z*direction*z;
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    boolean result = false;
+    if (other instanceof Plane) {
+      Plane that = (Plane) other;
+      result = direction.equals(result.direction) && intersect.equals(result.intersect);
+    }
+    return result;
+  }
 }
