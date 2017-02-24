e8f8958064f38ea0b8b32845cad7f6707ca5828b
ymosinzon <yotam@gatech.edu>
2017-02-24 15:01:41 -0500

    Add files via upload

---
+++ TangoSource/TangoSource/AdvHUD_TangoApp/src/main/java/com/projecttango/examples/java/hellomotiontracking/HUD_User.java
@@ -0,0 +1,20 @@
+package com.projecttango.examples.java.motiontracking;
+
+import com.google.atap.tangoservice.TangoPoseData;
+
+public class HUD_User {
+    private float[] position;
+    private float[] orientation;
+
+    public void update_pose(TangoPoseData pose) {
+        float[] translation = pose.getTranslationAsFloats();
+        float[] orientation = pose.getRotationAsFloats();
+
+        for (int i = 0; i < 3; i++) {
+            position[i] = translation[i];
+            this.orientation[i] = orientation[i];
+        }
+
+        this.orientation[3] = orientation[3];
+    }
+}
