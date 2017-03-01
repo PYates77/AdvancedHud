69b4b325fdc0ad70e4d35c8e3162c9d9ce1fe84a
RyanAStevens <ryan.antony.stevens@gmail.com>
2017-03-01 15:38:46 -0500

    added hashTable

---
+++ hello_depth_perception/src/main/java/com/projecttango/examples/java/hellodepthperception/TangoHashTable.java
@@ -0,0 +1,21 @@
+public class HashTable {
+	private class Node {
+		public 
+	}
+	
+	private Node[][] buckets;
+	private int num_buckets;
+	
+	public HashTable<T>(int nb) {
+		num_buckets = nb;
+		buckets = new T[nb][];
+	}
+	
+	private int hash(T) {
+		
+	}
+	
+	public void insert(T val) {
+		
+	}
+}
