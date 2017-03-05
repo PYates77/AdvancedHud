ec03afc2ef94065af16e8f108c7070be83ff1979
ymosinzon <yotam@gatech.edu>
2017-03-05 12:25:16 -0500

    Update Wall.java

--- hello_depth_perception/src/main/java/com/projecttango/examples/java/hellodepthperception/Wall.java
+++ hello_depth_perception/src/main/java/com/projecttango/examples/java/hellodepthperception/Wall.java
@@ -1,3 +1,5 @@
+package com.projecttango.examples.java.hellodepthperception;
+
 public class Wall {
     private Point edge1;
     private Point edge2;
@@ -11,7 +13,25 @@ public class Wall {
     }
     
     public void update(Cluster cluster) {
+        if (cluster == null)
+            return;
         
+        if (!isValid) {
+            edge2 = cluster.getCetroid();
+            isvalid = true;
+        }
+        
+        Point mid = new Point(0.5*edge1.x + 0.5*edge2.x, 0.5*edge1 + 0.5*edge2);
+        if (mid.dist2D(cluster.getCentroid()) > mid.dist2D(edge1)) {
+            if (edge1.dist2D(cluster.getCentroid()) > edge2.dist2D(cluster.getCentroid()))
+                edge2 = cluster.getCentroid();
+            else
+                edge1 = cluster.getCentroid();
+        }
+        plane.setDirection(new Point(plane.getDirection().x*0.5 + cluster.getPlane().getDirection().x*0.5, plane.getDirection().x*0.5
+                                     + cluster.getPlane().getDirection().y*0.5, plane.getDirection().z*0.5
+                                     + cluster..getPlane().getDirection().z*0.5)); // averaging the plane equation
+        plane.setShift(cluster.getPlane().getShift()*0.5 + plane.getShift()*0.5);
     }
     
     public double[] sendData() {
