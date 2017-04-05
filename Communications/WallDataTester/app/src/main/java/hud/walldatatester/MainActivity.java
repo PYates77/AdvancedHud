d01bfc97180c7e2dbb507f148320cefae13672ab
umbracatervae <akshay.nagendra@gmail.com>
2017-04-05 15:01:15 -0400

    Change to WallDataTester's Canned Data April 5th 2017

--- Communications/WallDataTester/app/src/main/java/hud/walldatatester/MainActivity.java
+++ Communications/WallDataTester/app/src/main/java/hud/walldatatester/MainActivity.java
@@ -26,12 +26,54 @@ public class MainActivity extends AppCompatActivity {
 
         //Generated Canned Data for transmission test
         ArrayList<Wall2D> testList = new ArrayList<Wall2D>();
-        for (int i=0; i <= 10; i++){
-            Wall2D w = new Wall2D(new Point(i+0.1,i+0.2,i+0.3));
-            w.addPoint(new Point(i+0.4,i+0.5,i+0.6));
-            testList.add(w);
-            Log.d("Wall Init","Created wall " + w.to_string());
-        }
+        Point A = new Point(90,400,0);
+        Point B = new Point(175,400,0);
+        Point C = new Point(90,140,0);
+        Point D = new Point(175,190,0);
+        Point E = new Point(-100,140,0);
+        Point F = new Point(270,190,0);
+        Point G = new Point(-100,90,0);
+        Point H = new Point(90,90,0);
+        Point I = new Point(90,40,0);
+        Point J = new Point(270,40,0);
+        Wall2D w0 = new Wall2D(A);
+        w0.addPoint(C);
+        Wall2D w1 = new Wall2D(C);
+        w1.addPoint(E);
+        Wall2D w2 = new Wall2D(G);
+        w2.addPoint(H);
+        Wall2D w3 = new Wall2D(H);
+        w3.addPoint(I);
+        Wall2D w4 = new Wall2D(I);
+        w4.addPoint(J);
+        Wall2D w5 = new Wall2D(J);
+        w5.addPoint(F);
+        Wall2D w6 = new Wall2D(F);
+        w6.addPoint(D);
+        Wall2D w7 = new Wall2D(D);
+        w7.addPoint(B);
+        testList.add(w0);
+        testList.add(w1);
+        testList.add(w2);
+        testList.add(w3);
+        testList.add(w4);
+        testList.add(w5);
+        testList.add(w6);
+        testList.add(w7);
+//        mWalls[0] = new Wall(A,C);
+//        mWalls[1] = new Wall(C,E);
+//        mWalls[2] = new Wall(G,H);
+//        mWalls[3] = new Wall(H,I);
+//        mWalls[4] = new Wall(I,J);
+//        mWalls[5] = new Wall(J,F);
+//        mWalls[6] = new Wall(F,D);
+//        mWalls[7] = new Wall(D,B);
+//        for (int i=0; i <= 10; i++){
+//            Wall2D w = new Wall2D(new Point(i+0.1,i+0.2,i+0.3));
+//            w.addPoint(new Point(i+0.4,i+0.5,i+0.6));
+//            testList.add(w);
+//            Log.d("Wall Init","Created wall " + w.to_string());
+//        }
         //this testlist is what needs to be sent over
 
         startBluetoothAdapter();
